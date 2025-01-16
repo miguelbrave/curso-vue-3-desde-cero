849f31d7e82aa299dd1ca3a9ed253134f863d344
Miguel Fernández Lama <miguel@wearebrave.net>
2025-01-16 13:29:28 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 const API = "https://api.github.com/users/";
+
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+          message: 'Hello vue!',
+          search: null,
+          result: null,
+          error: null,
+        }
+    },
+    methods: {
+        async doSearch(){
+            this.result = this.error = null
+            try{
+                const response = await fetch(API + this.search)
+                if(!response.ok) throw new Error("No encontrado")
+                const data = await response.json()
+                console.log(data)
+                this.result = true
+            }catch(error){
+                this.error = error
+            }finally{
+                this.search = null
+            }
+        }
+    }
+})
