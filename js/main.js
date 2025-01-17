b11d2e12b7f4cd7054dcdaa2376d67edd0f30cd2
Miguel Fernández Lama <miguel@wearebrave.net>
2025-01-17 13:55:59 +0100

    End

--- js/main.js
+++ js/main.js
@@ -1,49 +1,88 @@
 const API = "https://api.github.com/users/";
 
-
+const requestMaxTimeMs = 5000
 
 const app = Vue.createApp({
-    data() {
-        return {
-          message: 'Hello vue!',
-          search: null,
-          result: null,
-          error: null,
-          favorites: new Map(),
-        }
-    },
-    computed: {
-        isFavorite(){
-            return this.favorites.has(this.result.id)
-        },
-        allFavorites(){
-            return Array.from(this.favorites.values())
-        }
-    },
-    methods: {
-        async doSearch(){
-            this.result = this.error = null
-            try{
-                const response = await fetch(API + this.search)
-                if(!response.ok) throw new Error("No encontrado")
-                const data = await response.json()
-                console.log(data)
-                this.result = data
-            }catch(error){
-                this.error = error
-            }finally{
-                this.search = null
-            }
-        },
-        addFavorite(){
-            this.favorites.set(this.result.id, this.result)
-            this.updateLocalstorage()
-        },
-        removeFavorite(){
-            this.favorites.delete(this.result.id)
-        },
-        updateLocalstorage(){
-            window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
-        }
+  data() {
+    return {
+      search: null,
+      result: null,
+      error: null,
+      favorites: new Map()
+    };
+  },
+  created() {
+    const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"))
+    if (savedFavorites?.length) {
+      const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]))
+      this.favorites = favorites
     }
-})
+  },
+  computed: {
+    isFavorite() {
+      return this.favorites.has(this.result.login)
+    },
+    allFavorites() {
+      return Array.from(this.favorites.values())
+    }
+  },
+  methods: {
+    async doSearch() {
+      this.result = this.error = null
+
+      const foundInFavorites = this.favorites.get(this.search)
+
+      const shouldRequestAgain = (() => {
+        if (!!foundInFavorites) {
+          const { lastRequestTime } = foundInFavorites
+          const now = Date.now()
+          return (now - lastRequestTime) > requestMaxTimeMs
+        }
+        return false
+      })() // IIFE
+
+      if (!!foundInFavorites && !shouldRequestAgain) {
+        console.log("Found and we use the cached version")
+        return this.result = foundInFavorites
+      }
+
+      try {
+        console.log("Not found or cached version is too old")
+        const response = await fetch(API + this.search)
+        if (!response.ok) throw new Error("User not found")
+        const data = await response.json()
+        console.log(data)
+        this.result = data
+        foundInFavorites.lastRequestTime = Date.now()
+      } catch(error) {
+        this.error = error
+      } finally {
+        this.search = null
+      }
+    },
+
+    addFavorite() {
+      this.result.lastRequestTime = Date.now()
+      this.favorites.set(this.result.login, this.result)
+      this.updateStorage()
+    },
+
+    removeFavorite() {
+      this.favorites.delete(this.result.login)
+      this.updateStorage()
+    },
+
+    showFavorite(favorite) {
+      this.result = favorite
+    },
+
+    checkFavorite(login){
+        return this.result?.login === login
+    },
+
+    updateStorage() {
+      window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
+    }
+  }
+});
+
